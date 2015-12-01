477f3a59eeefd052383b0c60ea020c471cc4d589
JPGsProfil <j.gnauck@gmx.net>
2015-12-01 23:59:05 +0100

    return JSON working test with http://localhost:8080/api/getCardsJSON

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -7,13 +7,98 @@ import javax.ws.rs.core.MediaType;
  * Created by Jean on 01.12.2015.
  */
 
-@Path( "/firstTest" )
+@Path( "/getCardsJSON" )
 public class FirstTest
 {
     @GET
-    @Produces( MediaType.TEXT_PLAIN )
+    @Produces( MediaType.APPLICATION_JSON )
     public String getGreeting()
     {
-        return "Server Example";
+        String jsonTest = "{\n" +
+                "  \"cards\": [\n" +
+                "\t{\n" +
+                "\t  \"id\": 1,\n" +
+                "\t  \"name\": \"Brand Knabenkraut\",\n" +
+                "\t  \"Gattung\": \"Orchidee\",\n" +
+                "\t  \"Blütenfarbe\": \"weiß\",\n" +
+                "\t  \"essbar\": \"essbar\",\n" +
+                "\t  \"Typ\": \"Kraut\",\n" +
+                "\t  \"Klasse\": \"Zweikeimblättrige\",\n" +
+                "\t  \"image\": \"pfl_brand_knabenkraut\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 2,\n" +
+                "\t  \"name\": \"Holunder-Knabenkraut\",\n" +
+                "\t  \"Gattung\": \"Orchidee\",\n" +
+                "\t  \"Blütenfarbe\": \"gelb\",\n" +
+                "\t  \"essbar\": \"genießbar\",\n" +
+                "\t  \"Typ\": \"Strauch\",\n" +
+                "\t  \"Klasse\": \"Zweikeimblättrige\",\n" +
+                "\t  \"image\": \"pfl_holunder_knabenkraut\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 19,\n" +
+                "\t  \"name\": \"Erbse\",\n" +
+                "\t  \"Gattung\": \"Schmetterlingsblütler\",\n" +
+                "\t  \"Blütenfarbe\": \"grün\",\n" +
+                "\t  \"essbar\": \"genießbar\",\n" +
+                "\t  \"Typ\": \"Ranke\",\n" +
+                "\t  \"Klasse\": \"Zweikeimblättrige\",\n" +
+                "\t  \"image\": \"pfl_erbse\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 20,\n" +
+                "\t  \"name\": \"Fluegel-Ginster\",\n" +
+                "\t  \"Gattung\": \"Schmetterlingsblütler\",\n" +
+                "\t  \"Blütenfarbe\": \"gelb\",\n" +
+                "\t  \"essbar\": \"giftig\",\n" +
+                "\t  \"Typ\": \"Strauch\",\n" +
+                "\t  \"Klasse\": \"Zweikeimblättrige\",\n" +
+                "\t  \"image\": \"pfl_fluegel_ginster\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 21,\n" +
+                "\t  \"name\": \"Basilikum\",\n" +
+                "\t  \"Gattung\": \"Arzneipflanzen\",\n" +
+                "\t  \"Blütenfarbe\": \"weiß\",\n" +
+                "\t  \"essbar\": \"genießbar\",\n" +
+                "\t  \"Typ\": \"Kraut\",\n" +
+                "\t  \"Klasse\": \"Zweikeimblättrige\",\n" +
+                "\t  \"image\": \"pfl_basilikum\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 22,\n" +
+                "\t  \"name\": \"Bittersuesser Nachtschatten\",\n" +
+                "\t  \"Gattung\": \"Arzneipflanzen\",\n" +
+                "\t  \"Blütenfarbe\": \"rosa\",\n" +
+                "\t  \"essbar\": \"giftig\",\n" +
+                "\t  \"Typ\": \"kleiner Baum\",\n" +
+                "\t  \"Klasse\": \"Samenpflanzen\",\n" +
+                "\t  \"image\": \"pfl_bittersuesser_nachtschatten\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 23,\n" +
+                "\t  \"name\": \"Kleine Braunelle\",\n" +
+                "\t  \"Gattung\": \"Arzneipflanzen\",\n" +
+                "\t  \"Blütenfarbe\": \"blau\",\n" +
+                "\t  \"essbar\": \"essbar\",\n" +
+                "\t  \"Typ\": \"kleiner Baum\",\n" +
+                "\t  \"Klasse\": \"Zweikeimblättrige\",\n" +
+                "\t  \"image\": \"pfl_kleine_braunelle\"\n" +
+                "\t},\n" +
+                "\t{\n" +
+                "\t  \"id\": 24,\n" +
+                "\t  \"name\": \"Europaeische Eibe\",\n" +
+                "\t  \"Gattung\": \"Arzneipflanzen\",\n" +
+                "\t  \"Blütenfarbe\": \"lila\",\n" +
+                "\t  \"essbar\": \"giftig\",\n" +
+                "\t  \"Typ\": \"kleiner Baum\",\n" +
+                "\t  \"Klasse\": \"ka\",\n" +
+                "\t  \"image\": \"pfl_europaeische_eibe\"\n" +
+                "\t}\n" +
+                "  ]\n" +
+                "}";
+
+        return jsonTest;
     }
 }
