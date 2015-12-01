101c884259a10faefc0a0cb7404dc19288192bb5
JPGsProfil <j.gnauck@gmx.net>
2015-12-01 23:35:17 +0100

    second server first test

---
+++ src/main/java/ApplicationConfig.java
@@ -0,0 +1,20 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Jean on 01.12.2015.
+ */
+
+@ApplicationPath( "api" )
+public class ApplicationConfig extends Application
+{
+    @Override
+    public Set<Class<?>> getClasses()
+    {
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+        classes.add( FirstTest.class );
+        return super.getClasses();
+    }
+}
