c3d9971a81f8b41649a82091f609afed0397d84b
Jean <j.gnauck@gmx.net>
2016-01-09 11:57:29 +0100

    producing response after writing in db

--- src/main/java/database/Test_Hibernate.java
+++ src/main/java/database/Test_Hibernate.java
@@ -13,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -28,7 +29,7 @@ public class Test_Hibernate
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path( "test1" )
-    public String run2()
+    public Response run2()
     {
         Session testSessionFactory = HibernateUtil.getSessionFactory().openSession();
         testSessionFactory.beginTransaction();
@@ -39,7 +40,8 @@ public class Test_Hibernate
         String status = testSessionFactory.getStatistics().toString();
         testSessionFactory.getTransaction().commit();
         testSessionFactory.close();
-        return status;
+
+        return Response.ok().build();
     }
 
     @GET
